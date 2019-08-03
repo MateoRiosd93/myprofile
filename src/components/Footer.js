a5f6b9da7987aea85cc271898e7394373b6ad130
Mateo Rios Diaz <40178754+MateoRiosd93@users.noreply.github.com>
2019-08-03 15:50:02 -0500

    the footer is finished and with the home component. pending to finish the other components in the 320px view.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 import "../style/Footer.scss";
+import "../icons/style.css";
 
 function Footer() {
   return (
-    <div>
-      <h2 className="follow"> Aca van las redes sociales </h2>
+    <div className="container-follow">
+      <a
+        className="icon-follow"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.linkedin.com/in/mateo-rios-16458b186"
+      >
+        <span className="icon-linkedin2 icon" />
+      </a>
+      <a
+        className="icon-follow"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/MateoRiosd93"
+      >
+        <span className="icon-github icon" />
+      </a>
+      <a
+        className="icon-follow"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.instagram.com/mateorios.dev/"
+      >
+        <span className="icon-instagram icon" />
+      </a>
     </div>
   );
 }
