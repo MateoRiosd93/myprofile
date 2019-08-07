a0d626f2ffad6284f7ca9e3affe886047d2b378f
Mateo Rios Diaz <40178754+MateoRiosd93@users.noreply.github.com>
2019-08-07 00:18:10 -0500

    The About.js component with their respective styles was worked on and practically finished, it was also proposed to add styles to the Main.js to generta a better view of the route and footer content (better concealed the view spaces).

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,23 +5,29 @@ import About from "./About";
 import Experience from "./Experience";
 import Education from "./Education";
 import Skills from "./Skills";
-import Hobbies from "./Hobbies"
+import Hobbies from "./Hobbies";
 import Achievements from "./Achievements";
 import Footer from "./Footer";
 
+import "../style/Main.scss";
+
 function Router() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/About" component={About} />
-        <Route path="/Experience" component={Experience} />
-        <Route path="/Education" component={Education} />
-        <Route path="/Skills" component={Skills} />
-        <Route path="/Hobbies" component={Hobbies} />
-        <Route path="/Achievements" component={Achievements} />
-      </Switch>
-      <Footer/>
+    <div className="container-main">
+      <div className="container-route">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/Experience" component={Experience} />
+          <Route path="/Education" component={Education} />
+          <Route path="/Skills" component={Skills} />
+          <Route path="/Hobbies" component={Hobbies} />
+          <Route path="/Achievements" component={Achievements} />
+        </Switch>
+      </div>
+      <div className="contaienr-footer">
+        <Footer />
+      </div>
     </div>
   );
 }
